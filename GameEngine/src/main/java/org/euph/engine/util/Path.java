3594446618e03b5113c0336079a02e256ac1e534
Snoweuph <snow+github@euph.email>
2022-07-24 20:50:13 +0200

    Adding some base Package structure to project

---
+++ GameEngine/src/main/java/org/euph/engine/util/Path.java
@@ -0,0 +1,7 @@
+package org.euph.engine.util;
+
+public class Path {
+    public static final String ROOT = "/";
+    public static final String RESOURCES = ROOT + "res/";
+    public static  final String SHADERS = RESOURCES + "shaders/";
+}
