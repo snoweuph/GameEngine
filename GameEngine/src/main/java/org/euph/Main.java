e4990016ca6f51e009b1a5504172d1492769af49
Snoweuph <snow+github@euph.email>
2022-07-25 22:59:35 +0200

    Cleanup Main

--- GameEngine/src/main/java/org/euph/Main.java
+++ GameEngine/src/main/java/org/euph/Main.java
@@ -8,17 +8,27 @@ import static org.lwjgl.glfw.GLFW.glfwPollEvents;
 public class Main {
     public static void main(String[] args) {
 
+        //Initialize the Window Manager
         WindowManager.init();
 
+        //Create the Window
         Window win = WindowManager.createWindow(512, 512, true, true);
 
+        //Show the Window
         win.show(true);
 
+        //run the Main Loop
         while (!win.shouldClose()){
-            glfwPollEvents();
-            win.setTitle("DeltaTime is: " + win.getDelta());
-            win.update();
+            mainLoop(win);
         }
+
+        //Cleanup everything after closing
         WindowManager.cleanUp();
+    }
+
+    public static void mainLoop(Window win) {
+        glfwPollEvents();
+        win.setTitle("DeltaTime is: " + win.getDelta());
+        win.update();
     }
 }
