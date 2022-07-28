73192130e619bbc53565e65978ca6858c09db495
Snoweuph <snow+github@euph.email>
2022-07-28 22:17:21 +0200

    Improve ECS System
    updated it so that Components Inherintance will be looked onto and not ignored anymore

--- GameEngine/src/main/java/org/euph/engine/entityComponentSystem/Entity.java
+++ GameEngine/src/main/java/org/euph/engine/entityComponentSystem/Entity.java
@@ -4,19 +4,50 @@ import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 //TODO: JavaDoc
+
+/** This is the Definition of an Entity inside the ECS.
+ *
+ * @author snoweuph
+ * @version 1.0
+ */
 public class Entity {
 
     private boolean destroyed = false;
-
+    //Constructor
     public Entity(){
         EntityComponentSystem.createEntity(this);
     }
+    //Component Handling
+
+    /** Puts a Component onto this Entity.
+     *
+     * @param component the Component to put onto this.
+     *
+     * @author snoweuph
+     */
     public void putComponent(Component component){
         putComponent(component, new ArrayList<>());
     }
+
+    /** The Recursive Function for Putting a Component onto an Entity and also adding all required Components.
+     * This Function is wrapped by {@link #putComponent(Component)}.
+     *
+     * parameter `ArrayList<Class<? extends Component>> componentsOnEntityList` and return value `ArrayList<Class<? extends Component>>` are Important
+     * for keeping track of what Components are on the Entity without making Lots of Calls to {@link EntityComponentSystem#getComponentsOnEntity(Entity)},
+     * while trying to add all required Components.
+     *
+     * @throws IllegalStateException This Entity is Destroyed and Waiting for GC. It shouldn't have anymore references
+     *
+     * @param component The Component it should add.
+     * @param componentsOnEntityList The List of Components that are on the Entity.
+     *
+     * @return the List of the Classes of Components It added in that Cycle.
+     *
+     * @author snoweuph
+     */
     private ArrayList<Class<? extends Component>> putComponent(Component component, ArrayList<Class<? extends Component>> componentsOnEntityList){
         //If Already Destroyed Ignore and return
-        if(destroyed) throw new IllegalStateException("This Entity is Destroyed an Waiting for GC. It shouldn't have anymore references");
+        if(destroyed) throw new IllegalStateException("This Entity is Destroyed and Waiting for GC. It shouldn't have anymore references");
         //Remove the Old References from the ECS
         if(component.getEntity() != null) EntityComponentSystem.removeComponentReferences(component);
         //Set the new Entity
@@ -46,6 +77,31 @@ public class Entity {
         return addedComponents;
     }
 
+    /** Removes a Specific Component from the Entity.
+     *
+     * @param component the Component to remove.
+     *
+     * @author snoweuph
+     */
+    public void removeComponent(Component component){
+        //If Already Destroyed Ignore and return
+        if(destroyed) throw new IllegalStateException("This Entity is Destroyed and Waiting for GC. It shouldn't have anymore references");
+        EntityComponentSystem.removeComponentReferences(component);
+    }
+
+    /** Removes all Components of a Specific Type that are on an Entity
+     *
+     * @param componentClass the Type of Component that should be removed
+     *
+     * @author snoweuph
+     */
+    public void removeAllComponents(Class<?extends Component> componentClass){
+        //If Already Destroyed Ignore and return
+        if(destroyed) throw new IllegalStateException("This Entity is Destroyed and Waiting for GC. It shouldn't have anymore references");
+        EntityComponentSystem.removeComponentReferences(this, componentClass);
+    }
+
+    //Entity Handling
     /** This removes all Entries inside the ECS of this entity, so that the entity can wait for Garbage Collecting
      *
      * @implNote Remove all References to this Entity after running this.
@@ -66,5 +122,13 @@ public class Entity {
      */
     public boolean isDestroyed() {
         return destroyed;
+    }
+
+    /** @return a list of all Components that are on this Entity
+     *
+     * @author snoweuph
+     */
+    public ArrayList<Component> getComponents(){
+        return EntityComponentSystem.getComponentsOnEntity(this);
     }
 }
--- GameEngine/src/main/java/org/euph/engine/entityComponentSystem/EntityComponentSystem.java
+++ GameEngine/src/main/java/org/euph/engine/entityComponentSystem/EntityComponentSystem.java
@@ -2,6 +2,7 @@ package org.euph.engine.entityComponentSystem;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
 //TODO: JavaDoc
 //TODO: Implement the rest of the features
@@ -28,32 +29,43 @@ public class EntityComponentSystem {
     }
     protected static void addComponentReferences(Component component, Entity entity){
         //References to the Components Class
-        Class<? extends Component> componentClass= component.getClass();
-        //Test if an Arraylist for this Component already exists, if not, create one.
-        if(!componentInstancesMap.containsKey(componentClass)){
-            componentInstancesMap.put(componentClass, new ArrayList<>());
+        Class<? extends Component> componentClass = component.getClass();
+        ArrayList<Class<? extends Component>> targets = getComponentInheritanceList(componentClass, new ArrayList<>());
+        targets.add(componentClass);
+        //Test if an Arraylist for the targeted Components already exists, if not, create one.
+        for(Class<? extends Component> target : targets){
+            if(!componentInstancesMap.containsKey(target)){
+                componentInstancesMap.put(target, new ArrayList<>());
+            }
+
         }
         //Add the Entry to the Entity-Component Map
         ArrayList<Component> entityComponentsList = entityComponentMap.get(entity);
         entityComponentsList.add(component);
         entityComponentMap.put(entity, entityComponentsList);
-        //Append the Component Instance to the appropriate ArrayList inside the HashMap;
-        ArrayList<Component> instanceList = componentInstancesMap.get(componentClass);
-        instanceList.add(component);
-        componentInstancesMap.put(componentClass, instanceList);
+        //Append the Component Instance to the appropriate ArrayLists inside the HashMap;
+        for(Class<? extends Component> target : targets){
+            ArrayList<Component> instanceList = componentInstancesMap.get(target);
+            instanceList.add(component);
+            componentInstancesMap.put(target, instanceList);
+        }
     }
     protected static void removeComponentReferences(Component component){
         //References to the Components Class
-        Class<? extends Component> componentClass= component.getClass();
+        Class<? extends Component> componentClass = component.getClass();
+        ArrayList<Class<? extends Component>> targets = getComponentInheritanceList(componentClass, new ArrayList<>());
+        targets.add(componentClass);
         //Test if this Component has Any Entries, if not return
         if(!componentInstancesMap.containsKey(componentClass)) return;
         //Remove References from instances Map, remove Entry if empty
-        ArrayList<Component> instanceList = componentInstancesMap.get(componentClass);
-        instanceList.remove(component);
-        if(instanceList.size() <= 0){
-            componentInstancesMap.remove(componentClass);
-        }else {
-            componentInstancesMap.put(componentClass, instanceList);
+        for(Class<? extends Component> target : targets){
+            ArrayList<Component> instanceList = componentInstancesMap.get(target);
+            instanceList.remove(component);
+            if(instanceList.size() <= 0){
+                componentInstancesMap.remove(target);
+            }else {
+                componentInstancesMap.put(target, instanceList);
+            }
         }
         //Remove References from the Entity-Component Map
         Entity entity = component.getEntity();
@@ -61,8 +73,63 @@ public class EntityComponentSystem {
         entityComponentsList.remove(component);
         entityComponentMap.put(entity, entityComponentsList);
     }
+    protected static void removeComponentReferences(Entity entity, Class<? extends Component> componentClass){
+        //Test if Entity has any of these Components
+        ArrayList<Component> entityComponents = entityComponentMap.get(entity);
+        ArrayList<Class<? extends Component>> targets = getComponentInheritanceList(componentClass, new ArrayList<>());
+        targets.add(componentClass);
+        //Create HasMap to Temporarily store what needs to be Removed
+        HashMap<Class<? extends Component>, ArrayList<Component>> componentsToRemove = new HashMap<>();
+        for (Component component : entityComponents){
+            if(!targets.contains(component.getClass()))continue;
+            if(!componentsToRemove.containsKey(component.getClass())){
+                componentsToRemove.put(component.getClass(), new ArrayList<>());
+            }
+            ArrayList<Component> removeList = componentsToRemove.get(component.getClass());
+            removeList.add(component);
+            componentsToRemove.put(component.getClass(), removeList);
+        }
+        if(componentsToRemove.size() <= 0)return;
+        //Remove Component Instance References
+        for(Class<? extends Component> key : componentsToRemove.keySet()){
+            ArrayList<Component> instanceList = componentInstancesMap.get(key);
+            for(Component component : componentsToRemove.get(key)){
+                instanceList.remove(component);
+            }
+            if(instanceList.size() <= 0){
+                componentInstancesMap.remove(key);
+            }else {
+                componentInstancesMap.put(key, instanceList);
+            }
+        }
+        //Remove References from the Entity Component
+        ArrayList<Component> entityComponentsList = entityComponentMap.get(entity);
+        for(Class<? extends Component> key : componentsToRemove.keySet()){
+            for (Component component : componentsToRemove.get(key)){
+                entityComponentsList.remove(component);
+            }
+            entityComponentMap.put(entity, entityComponentsList);
+
+        }
+    }
     protected static ArrayList<Component> getComponentsOnEntity(Entity entity){
         return entityComponentMap.get(entity);
     }
 
+    //Utility functions
+    //TODO: Make Wrapper Function, that Caches the Result of this, to speed up the ECS
+    private static ArrayList<Class<? extends Component>> getComponentInheritanceList(Class<?extends Component> componentType, ArrayList<Class<? extends Component>> foundComponents){
+        //Get SupperClass
+        Class<?> superClass = componentType.getSuperclass();
+        //If SuperClass is directly Component.class, it's a first Level Component
+        if(superClass == Component.class) return foundComponents;
+        //Cast current class to right Type and Add it to the list
+        if(Component.class.isAssignableFrom(superClass)){
+            @SuppressWarnings("unchecked")
+            Class<? extends Component> superClassExtendingComponent = (Class<? extends Component>) superClass;
+            foundComponents.add(superClassExtendingComponent);
+            return  getComponentInheritanceList(superClassExtendingComponent, foundComponents);
+        }
+        throw new IllegalStateException("How did you do this?");
+    }
 }
