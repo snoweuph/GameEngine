7b486b7df1057272da09f1a3732de603ccb453ce
Snoweuph <snow+github@euph.email>
2022-07-28 12:30:57 +0200

    makeing unsafe code Save

--- GameEngine/src/main/java/org/euph/engine/entityComponentSystem/Entity.java
+++ GameEngine/src/main/java/org/euph/engine/entityComponentSystem/Entity.java
@@ -1,6 +1,7 @@
 package org.euph.engine.entityComponentSystem;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 //TODO: JavaDoc
 public class Entity {
@@ -18,13 +19,17 @@ public class Entity {
         if(destroyed) throw new IllegalStateException("This Entity is Destroyed an Waiting for GC. It shouldn't have anymore references");
         //Remove the Old References from the ECS
         if(component.getEntity() != null) EntityComponentSystem.removeComponentReferences(component);
+        //Set the new Entity
+        component.setEntity(this);
+        //Add the References to the ECS
+        EntityComponentSystem.addComponentReferences(component, this);
         //Create a list of Existing Components on the Entity, if not an empty one was passed
         ArrayList<Class<? extends Component>> componentsOnEntity = componentsOnEntityList;
         if(componentsOnEntity.size() <= 0){
             ArrayList<Class<? extends Component>> tempComponentsOnEntity = componentsOnEntity;
             EntityComponentSystem.getComponentsOnEntity(this).forEach(c -> tempComponentsOnEntity.add(c.getClass()));
             //Filter of Duplicate Entries.
-            componentsOnEntity = (ArrayList<Class<? extends Component>>) tempComponentsOnEntity.stream().distinct();
+            componentsOnEntity = (ArrayList<Class<? extends Component>>) tempComponentsOnEntity.stream().distinct().collect(Collectors.toList());
         }
         //Create a List to store all Added Components
         ArrayList<Class<? extends Component>> addedComponents = new ArrayList<>();
@@ -33,15 +38,10 @@ public class Entity {
             //Continue if Required Component is on Entity
             if(componentsOnEntity.contains(requiredComponent.getClass())) continue;
             //Run this Function Recursive for the Required Component, add the Added Components to the list of Components on this Entity and then remove duplicates
-            //TODO: Implement some Infinite loop Safety
             componentsOnEntity.addAll(putComponent(requiredComponent, componentsOnEntity));
             addedComponents.add(requiredComponent.getClass());
-            componentsOnEntity = (ArrayList<Class<? extends Component>>) componentsOnEntity.stream().distinct();
+            componentsOnEntity = (ArrayList<Class<? extends Component>>) componentsOnEntity.stream().distinct().collect(Collectors.toList());
         }
-        //Set the new Entity
-        component.setEntity(this);
-        //Add the References to the ECS
-        EntityComponentSystem.addComponentReferences(component, this);
         //Return list of added Components
         return addedComponents;
     }
