19e977aa181fe9fa8f1c92f9beef4d29bb573957
Snoweuph <snow+github@euph.email>
2022-07-28 10:50:42 +0200

    Optimized Recursive putComponent()
    Optimized Recursive putComponent()  to do less calls and push more data through recursiove calls

--- GameEngine/src/main/java/org/euph/engine/entityComponentSystem/Entity.java
+++ GameEngine/src/main/java/org/euph/engine/entityComponentSystem/Entity.java
@@ -10,18 +10,22 @@ public class Entity {
     public Entity(){
         EntityComponentSystem.createEntity(this);
     }
-
-    public ArrayList<Class<? extends Component>> putComponent(Component component){
+    public void putComponent(Component component){
+        putComponent(component, new ArrayList<>());
+    }
+    private ArrayList<Class<? extends Component>> putComponent(Component component, ArrayList<Class<? extends Component>> componentsOnEntityList){
         //If Already Destroyed Ignore and return
         if(destroyed) throw new IllegalStateException("This Entity is Destroyed an Waiting for GC. It shouldn't have anymore references");
         //Remove the Old References from the ECS
         if(component.getEntity() != null) EntityComponentSystem.removeComponentReferences(component);
-        //Create a list of Existing Components on the Entity
-        ArrayList<Class<? extends Component>> componentsOnEntity = new ArrayList<>();
-        ArrayList<Class<? extends Component>> tempComponentsOnEntity = componentsOnEntity;
-        EntityComponentSystem.getComponentsOnEntity(this).forEach(c -> tempComponentsOnEntity.add(c.getClass()));
-        //Filter of Duplicate Entries.
-        componentsOnEntity = (ArrayList<Class<? extends Component>>) tempComponentsOnEntity.stream().distinct();
+        //Create a list of Existing Components on the Entity, if not an empty one was passed
+        ArrayList<Class<? extends Component>> componentsOnEntity = componentsOnEntityList;
+        if(componentsOnEntity.size() <= 0){
+            ArrayList<Class<? extends Component>> tempComponentsOnEntity = componentsOnEntity;
+            EntityComponentSystem.getComponentsOnEntity(this).forEach(c -> tempComponentsOnEntity.add(c.getClass()));
+            //Filter of Duplicate Entries.
+            componentsOnEntity = (ArrayList<Class<? extends Component>>) tempComponentsOnEntity.stream().distinct();
+        }
         //Create a List to store all Added Components
         ArrayList<Class<? extends Component>> addedComponents = new ArrayList<>();
         //Iterate over Required Components
@@ -30,7 +34,7 @@ public class Entity {
             if(componentsOnEntity.contains(requiredComponent.getClass())) continue;
             //Run this Function Recursive for the Required Component, add the Added Components to the list of Components on this Entity and then remove duplicates
             //TODO: Implement some Infinite loop Safety
-            componentsOnEntity.addAll(putComponent(requiredComponent));
+            componentsOnEntity.addAll(putComponent(requiredComponent, componentsOnEntity));
             addedComponents.add(requiredComponent.getClass());
             componentsOnEntity = (ArrayList<Class<? extends Component>>) componentsOnEntity.stream().distinct();
         }
