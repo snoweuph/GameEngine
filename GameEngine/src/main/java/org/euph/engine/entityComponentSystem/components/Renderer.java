ee9d1c04a480efa571e3689d7c36a1d54383a9eb
Snoweuph <snow+github@euph.email>
2022-07-30 14:57:54 +0200

    Adding TODO

--- GameEngine/src/main/java/org/euph/engine/entityComponentSystem/components/Renderer.java
+++ GameEngine/src/main/java/org/euph/engine/entityComponentSystem/components/Renderer.java
@@ -5,11 +5,12 @@ import org.euph.engine.entityComponentSystem.Component;
 import java.util.ArrayList;
 import java.util.List;
 
+//TODO: this
+//TODO: JavaDoc
 abstract public class Renderer extends Component {
     @Override
     protected List<Component> getRequiredComponents() {
         List<Component> components = new ArrayList<>();
-        components.add(new Transform());
         return components;
     }
 }
--- GameEngine/src/main/java/org/euph/engine/entityComponentSystem/systems/render/RenderSystem.java
+++ GameEngine/src/main/java/org/euph/engine/entityComponentSystem/systems/render/RenderSystem.java
@@ -1,4 +1,7 @@
 package org.euph.engine.entityComponentSystem.systems.render;
 
+//TODO: this
+//TODO: JavaDoc
 public class RenderSystem {
+
 }
